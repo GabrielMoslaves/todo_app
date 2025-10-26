b488410bdc52440f289262d6389ea7e85abb949e
Gabriel <gabrielmoslaves@gmail.com>
2025-10-26 18:18:44 -0300

    CORS

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const app = express();
 
 app.use(express.json());
 
+// Middleware CORS manual
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Em produção, substitua '*' pelo domínio do seu cliente
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  // Responder a requisições OPTIONS (preflight)
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use("/tasks", taskRoutes);
 app.use("/users", usersRoutes);
 app.use("/auth", authRoutes);
